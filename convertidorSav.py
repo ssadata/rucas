bfd46af4898fe63500eb9aa51d5923ecfd0c5f18
Simon Salazar Albornoz <39565392+ssalazara@users.noreply.github.com>
2020-04-28 11:28:20 -0400

    Update convertidorSav.py

--- convertidorSav.py
+++ convertidorSav.py
@@ -16,4 +16,4 @@ df0, meta = prst.read_sav('/home/ubuntu/Rucas/data/sav/base.sav')
 
 
 ############# 3)  #####################################################
-df0.to_csv('home/ubuntu/Rucas/data/csv/base.csv', sep=',', encoding='utf-8', index = False)
+df0.to_csv('/data/csv/base.csv', sep=',', encoding='utf-8', index = False)
