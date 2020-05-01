4af8346e2c7ef647a828d34541b0d1f8bc79dc99
Simon Salazar Albornoz <39565392+ssalazara@users.noreply.github.com>
2020-04-30 23:34:57 -0400

    Update consulta_compleja3.py

--- consulta_compleja3.py
+++ consulta_compleja3.py
@@ -24,17 +24,20 @@ for file in os.listdir(dir_path):
     bases[file.split('.')[0]] = db
 
 
-#Pregunta n° 1:
-      
 for base in bases:
           bases[base]["folio_unico"] = bases[base]["folio_villa"].astype('str') + '-' + bases[base]["folio_vivienda"].astype('str')
           
+  
+#PREGUNTA   
           
 cols = ["folio_unico", "P15_1_1B", "P15_1_2", "P15_1_3A", "P15_1_3B", "P15_2_1B", "P15_2_2", "P15_2_3A", "P15_2_3B", "P15_3_1B", "P15_3_2", "P15_3_3A", "P15_3_3B", "P15_1_W1", "P15_2_W1", "P15_3_W1"]
           
-df1 = bases["w1_bdm_p"].loc[:, cols]
+df1 = bases["w1_bdm_p"].loc[:, ["folio_villa", "folio_vivienda"] + cols]
+          
 df2 = bases["w2_bdm_p"].loc[:, cols]
+          
 df3 = bases["w3_bdm_p"].loc[:, cols]
+          
 df = (df1.append([df2, df3]))
           
 df.to_csv('/home/ubuntu/Rucas/data/consultas/01may_Cc3.csv', sep=',', float_format = '%.12g', encoding='utf-8', index = False)
