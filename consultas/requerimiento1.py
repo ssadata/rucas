8ad361f7f874a2b08b6dbac6b9131f0cf24b241b
Simon Salazar Albornoz <39565392+ssalazara@users.noreply.github.com>
2020-05-26 08:19:12 -0400

    Update requerimiento1.py

--- consultas/requerimiento1.py
+++ consultas/requerimiento1.py
@@ -1,3 +1,31 @@
+#########################   PACKAGES / LIBRERIAS   ######################
+
+import os
+import json
+import pandas as pd
+import pyreadstat
+
+########################## DIR_PATH'S / DIRECTORIOS #######################
+
+main_path = "/home/haze/Documentos/Programa/AtomProjects/RUCAS/tablas/TEST/GitHub/DIRECTORIOS/CSV"
+table_paths =["/BDM_CSV/", "/MB_CSV/"]
+
+##########################   DB  /  BASE DE DATOS   #######################
+
+bases = {}
+for path in table_paths:
+    current_path = main_path + path
+    for file in os.listdir(current_path):
+        print(f"procesando {file.split('.')[0]}")
+        db = pd.read_csv(current_path+file, sep=',', thousands=".", header = 0, error_bad_lines=False, encoding="ISO-8859-1")
+        if db.columns.shape[0] < 2:
+            db = pd.read_csv(current_path+file, sep=';', thousands=",", header = 0, error_bad_lines=False, encoding="ISO-8859-1")
+        bases[file.split('.')[0]] = db
+
+#########################    QUERIES  /  CONSULTAS    ######################
+#**************************************************************************#
+##################################   N °   1   #############################
+
 # 1. Ejemplo de unión de dos bases de datos con distinto número de filas: BBDD Botones y BDM W2.
 
 # a. Generar una base de datos que tenga las variables de BBDD de Botones y de los datos de la encuesta correspondientes a esas viviendas, exportarlas como CSV,
