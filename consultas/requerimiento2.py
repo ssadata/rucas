75f99f01dfc67450eebb9d6efcc08293b7b337b9
Simon Salazar Albornoz <39565392+ssalazara@users.noreply.github.com>
2020-05-26 09:41:51 -0400

    Create requerimiento2.py

---
+++ consultas/requerimiento2.py
@@ -0,0 +1,33 @@
+#########################   PACKAGES / LIBRERIAS   ######################
+
+import os
+import json
+import pandas as pd
+import pyreadstat
+
+########################## DIR_PATH'S / DIRECTORIOS #######################
+
+main_path = "/home/ubuntu/Rucas/data/dir_path/csv"
+table_paths =["/bdm/", "/mb/"]
+
+##########################   DB  /  BASE DE DATOS   #######################
+
+bases = {}
+for path in table_paths:
+    current_path = main_path + path
+    for file in os.listdir(current_path):
+        print(f"procesando {file.split('.')[0]}")
+        db = pd.read_csv(current_path+file, sep=',', thousands=".", header = 0, error_bad_lines=False, encoding="ISO-8859-1")
+        if db.columns.shape[0] < 2:
+            db = pd.read_csv(current_path+file, sep=';', thousands=",", header = 0, error_bad_lines=False, encoding="ISO-8859-1")
+        bases[file.split('.')[0]] = db
+
+
+##########################   ID  /  IDENTIFICADOR   #######################
+              
+for base in bases:
+    bases[base]["folio_unico"] = bases[base]["folio_villa"].astype('str') + '-' + bases[base]["folio_vivienda"].astype('str')              
+
+#########################    QUERIES  /  CONSULTAS    ######################
+#**************************************************************************#
+##################################   N °   2   #############################
