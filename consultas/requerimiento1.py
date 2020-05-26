76dc9966504e74cfc796e215208817f334805fff
Simon Salazar Albornoz <39565392+ssalazara@users.noreply.github.com>
2020-05-26 11:03:45 -0400

    Update requerimiento1.py

--- consultas/requerimiento1.py
+++ consultas/requerimiento1.py
@@ -45,7 +45,7 @@ result = (df1.join(df2.set_index('folio_vivienda'), how = "left", on = 'folio_vi
 print(result.head())
               
 result.drop_duplicates(keep='first')              
-result.to_csv('/home/ubuntu/Rucas/data/dir_path/csv/tab/requerimiento1.csv', sep=',', encoding='utf-8', index = False)
+result.to_csv('/home/ubuntu/Rucas/data/dir_path/csv/tab/requerimiento1_1.csv', sep=',', encoding='utf-8', index = False)
               
               
 # b. Montar nuevamente en el sistema,
