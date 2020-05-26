c1d4ea51b051a25d131acf7d27b8a4da7b8636cc
Simon Salazar Albornoz <39565392+ssalazara@users.noreply.github.com>
2020-05-26 00:10:29 -0400

    Create savToCsv.py

---
+++ conversorSav/savToCsv.py
@@ -0,0 +1,23 @@
+
+#######################################################################
+
+##### El presente scrip cumple con la función de convertir los archivos de SPSS (`.sav`) en CSV ######
+
+
+# 1) Para cumplir dicho objetivo leemos desde el directorio de minio el cual tiene alojados los archivos `.sav` 
+#[/home/ubuntu/Rucas/data/sav/]
+
+# 2) Luego de eso debemos agregar el nombre del archivo que transformaremos [reemplazar `<filename>`]
+
+# 3) Por último le asignamos nombre al archivo que guardaremos como `.csv` [reemplazar `<name>`]
+
+import pyreadstat as prst
+# 1) #################################################### 2)  #########
+df0, meta = prst.read_sav('/home/ubuntu/Rucas/data/sav/BdM Participantes W1W2W3 20 05 07.sav')
+
+
+############# 3)  #####################################################
+df0.to_csv('/home/ubuntu/Rucas/data/bases/BdM Participantes W1W2W3 20 05 07.csv', sep=',', float_format = '%.12g', encoding='utf-8', index = False)
+################################################ , sep=',', float_format nos quita los decimales '12.0' a '12' #### 
+
+# En caso de no poder ejecutar este documento: http://ezcsv2sav.com/about/ " 
