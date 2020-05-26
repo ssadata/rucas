b05c126697e6d4ac1153a89ef623b21e4c08e363
Simon Salazar Albornoz <39565392+ssalazara@users.noreply.github.com>
2020-05-26 08:17:45 -0400

    Create requerimiento1.py

---
+++ consultas/requerimiento1.py
@@ -0,0 +1,6 @@
+# 1. Ejemplo de unión de dos bases de datos con distinto número de filas: BBDD Botones y BDM W2.
+
+# a. Generar una base de datos que tenga las variables de BBDD de Botones y de los datos de la encuesta correspondientes a esas viviendas, exportarlas como CSV,
+# b. Montar nuevamente en el sistema,
+# c. Ver en Metabase una tabla cruzada de: Media de temperatura (T_Ddia_prom) y humedad (H_Ddia_prom) con número de integrantes de la vivienda (total_integrantes_vivienda).
+
