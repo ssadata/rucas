de4a7dcec2f4efc0f434821dff0cbef8441d7db3
Simon Salazar Albornoz <39565392+ssalazara@users.noreply.github.com>
2020-05-26 09:05:21 -0400

    Update con_mb.py

--- conectores/con_mb.py
+++ conectores/con_mb.py
@@ -42,6 +42,7 @@ for file in os.listdir(dir_path):
     with open(f"{dir_pathj+file.split('.')[0]}.json", "r") as f:    
         cols = json.load(f)
     db = db.rename(columns=cols)
+	      
     if db.columns.shape[0] > 1: 
 	db.to_sql(file.split(".")[0], conn, index = False)
 	
