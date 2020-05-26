538615df6fa996b7fccbb0f3e90c69d8290289e3
Simon Salazar Albornoz <39565392+ssalazara@users.noreply.github.com>
2020-05-26 00:30:28 -0400

    Update con_mb.py

--- conectores/con_mb.py
+++ conectores/con_mb.py
@@ -1,4 +1,5 @@
 #
+#########################   PACKAGES / LIBRERIAS   ######################
 
 import os
 import json
@@ -6,6 +7,7 @@ import pandas as pd
 import sqlalchemy
 from sqlalchemy import create_engine
 
+#########################   ENGINE / CONECTOR   ######################
 
 POSTGRES_ADDRESS = 'localhost' ## INSERT YOUR DB ADDRESS IF IT'S NOT ON PANOPLY
 POSTGRES_PORT = '5432'         ## CONNECT WITH THE PORT
@@ -19,21 +21,27 @@ postgres_str = f"postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_
 # Conector que servirá de motor de búsqueda
 conn = create_engine(postgres_str)	
 
-dir_path = "/home/ubuntu/Rucas/data/BDM/"
-#dir_pathj = "/home/ubuntu/Rucas/data/csv_json/json"
+#########################   DIR_PATH'S / DIRECTORIOS   ######################
+
+dir_path = "/home/ubuntu/Rucas/data/dir_path/csv/mb"
+dir_pathj = "/home/ubuntu/Rucas/data/dir_path/json/mb"
+
+#########################   DB / BASE DE DATOS   ######################
 
 for file in os.listdir(dir_path):
     print(f"Procesando {file}")
+
     db = pd.read_csv(dir_path+file, sep=',', thousands=".", header = 0, error_bad_lines=False, encoding="ISO-8859-1")
-### Observacion; la condicion (IF) genera un bug en la carga de las tablas con sep=';', por el momento todas las tablas utilizan sep=';'
+##   Observacion; todas las tablas deben utilizar sep=';'
+
     if db.columns.shape[0] < 2:
         db = pd.read_csv(dir_path+file, sep=';', thousands=",", header = 0, error_bad_lines=False, encoding="ISO-8859-1")
 
+#########################   JSON / ETIQUETAS  ######################
 
-### Lectura y reemplazo de columnas mediante archivos .json
-
- #   with open(f"{dir_pathj+file.split('.')[0]}.json", "r") as f:    
- #       cols = json.load(f)
- #   db = db.rename(columns=cols)
+    with open(f"{dir_pathj+file.split('.')[0]}.json", "r") as f:    
+        cols = json.load(f)
+    db = db.rename(columns=cols)
 if db.columns.shape[0] > 1: 
 	db.to_sql(file.split(".")[0], conn, index = False)
+	
