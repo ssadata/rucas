557b77d1f6dc7bb3297dec5ee4a5a11d1891d20b
Simon Salazar Albornoz <39565392+ssalazara@users.noreply.github.com>
2020-05-27 08:36:51 -0400

    Update requerimiento2.py

--- consultas/requerimiento2.py
+++ consultas/requerimiento2.py
@@ -5,7 +5,6 @@ import json
 import pandas as pd
 import pyreadstat
 import re
-
 ########################## DIR_PATH'S / DIRECTORIOS #######################
 
 main_path = "/home/ubuntu/Rucas/data/dir_path/csv"
@@ -23,9 +22,8 @@ for path in table_paths:
             db = pd.read_csv(current_path+file, sep=';', thousands=",", header = 0, error_bad_lines=False, encoding="ISO-8859-1")
         bases[file.split('.')[0]] = db
 
-
 ##########################   ID  /  IDENTIFICADOR   #######################
-              
+
 for base in bases:
     bases[base]["folio_unico"] = bases[base]["folio_villa"].astype('str') + '-' + bases[base]["folio_vivienda"].astype('str')              
 
@@ -35,15 +33,15 @@ for base in bases:
 
 # 1. Unir bases de datos "hacia el lado”: Tenemos 3 olas de BDM.
 ##########################     DATAFRAMES      #############################
-cols1 = ["estado_intervencion", "telefonica"]
+cols1 = ["folio_vivienda", "folio_unico", "estado_intervencion", "telefonica"]
 cols2 = ["cambio_vivienda", "fuera_villa", "hogar_dividido", "cambio_inf_baseline", "cambio_inf_prev", "fuera_villa", "hogar_dividido"]
-              
+
 df1 = bases["w1_bdm_bbdd_hh_20_04_24"].loc[:, cols1]
 df2 = bases["w2_bdm_bbdd_hh_20_04_24"].loc[:, cols1 + cols2]
 df3 = bases["w3_bdm_bbdd_hh_20_04_24"].loc[:, cols1 + cols2]
             
 ################       ELABORACION DE REQUERIMIENTO      ###################              
-result = (df1.join(df2.set_index('folio_unico'), how = "left", on = 'folio_unico', lsuffix ="", rsuffix = "_01"))
+result = (pd.concat([df1, df2, df3],axis = 1, sort = False))
 
 ################    ELIMINACION DE VALORES DUPLICADOS    ################### 
 #f_result = result.drop_duplicates('folio_vivienda')              
